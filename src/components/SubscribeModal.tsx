afa53aebfa65c0ec9ce375dc85704b3888614966
Prabin Subedi <42871240+prabincankod@users.noreply.github.com>
2026-03-19 23:36:22 +0545

    changes in landing page plss.

--- src/components/SubscribeModal.tsx
+++ src/components/SubscribeModal.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+
 
 export const SubscribeModal = () => {
   return (
-    <div>Subscribe me lovely</div>
+    <div>
+      <h2 className='text-center text-2xl font-semibold'>
+        Join our Waitlist
+      </h2>
+      <form action="https://forminit.com/f/lea8hdeo1i7/" method='POST' className='flex flex-col'>
+        <fieldset className="d-fieldset">
+          <legend className="d-fieldset-legend ">What is your name?</legend>
+          <input name="name" type="text" className="d-input w-full" placeholder="Your name please" />
+        </fieldset>
+
+        <fieldset className="d-fieldset">
+          <legend className="d-fieldset-legend ">What is your email?</legend>
+          <input name="email" type="email" className="d-input w-full" placeholder="Your Best Email" />
+        </fieldset>
+
+        <button type="submit" className="d-btn d-btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
   )
 }
