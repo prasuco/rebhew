d4cd52e6b7945273e096eb4a4503976723db665c
Prabin Subedi <42871240+prabincankod@users.noreply.github.com>
2026-03-20 11:26:41 +0545

    feat: used web3forms for waitlist.

--- src/components/SubscribeModal.tsx
+++ src/components/SubscribeModal.tsx
@@ -1,12 +1,12 @@
-
-
 export const SubscribeModal = () => {
   return (
     <div>
       <h2 className='text-center text-2xl font-semibold'>
         Join our Waitlist
       </h2>
-      <form action="https://forminit.com/f/lea8hdeo1i7/" method='POST' className='flex flex-col gap-2'>
+      <form action="https://api.web3forms.com/submit" method='POST' className='flex flex-col gap-2'>
+        <input type="hidden" name="access_key" value="951f97d2-662c-4c34-80c4-cfac2beaeb8a" />
+
         <fieldset className="d-fieldset">
           <legend className="d-fieldset-legend ">What is your name?</legend>
           <input name="name" type="text" className="d-input w-full" placeholder="Your name please" />
